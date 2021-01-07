df29581b4e6c672cb3f6b146dc0a14aea321a84c
Amy Paschke <pasch049@umn.edu>
2021-01-07 15:11:56 -0600

    adding to heroku

--- public/assets/js/cats.js
+++ public/assets/js/cats.js
@@ -1,60 +1,53 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".change-sleep").on("click", function(event) {
+$(function () {
+  $(".change-sleep").on("click", function (event) {
     var id = $(this).data("id");
     var newSleep = $(this).data("newsleep");
 
     var newSleepState = {
-      sleepy: newSleep
+      sleepy: newSleep,
     };
 
     // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newSleepState
-    }).then(
-      function() {
-        console.log("changed sleep to", newSleep);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+      data: newSleepState,
+    }).then(function () {
+      // Reload the page to get the updated list
+      location.reload();
+    });
   });
 
-  $(".create-form").on("submit", function(event) {
+  $(".create-form").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newCat = {
+    var newBurger = {
       name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
+      sleepy: $("[name=sleepy]:checked").val().trim(),
     };
 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burgers", {
       type: "POST",
-      data: newCat
-    }).then(
-      function() {
-        console.log("created new cat");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+      data: newCat,
+    }).then(function () {
+      console.log("created new cat");
+      // Reload the page to get the updated list
+      location.reload();
+    });
   });
 
-  $(".delete-cat").on("click", function(event) {
+  $(".delete-cat").on("click", function (event) {
     var id = $(this).data("id");
 
     // Send the DELETE request.
     $.ajax("/api/cats/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted cat", id);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+      type: "DELETE",
+    }).then(function () {
+      console.log("deleted cat", id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
   });
 });
