4e794ca9d7bb6b2db735146d7336ec16dea65fc4
Amy Paschke <pasch049@umn.edu>
2021-01-07 15:55:53 -0600

    added more info to JS files

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,14 +1,10 @@
-var express = require("express");
+const express = require("express");
+const burger = require("../models/burger.js");
 
-var router = express.Router();
-
-// Import the model (burger.js) to use its database functions.
-var cat = require("../models/burger.js");
-
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function (req, res) {
-  cat.all(function (data) {
-    var hbsObject = {
+let router = express.Router();
+router.get("/", (req, res) => {
+  burger.selectAll((data) => {
+    let hbsObject = {
       burgers: data,
     };
     console.log(hbsObject);
@@ -16,50 +12,30 @@ router.get("/", function (req, res) {
   });
 });
 
-router.post("/api/burgers", function (req, res) {
-  cat.create(
-    ["name", "devoured"],
-    [req.body.name, req.body.devoured],
-    function (result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-    }
-  );
-});
-
-router.put("/api/burgers/:id", function (req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.update(
-    {
-      devoured: req.body.devoured,
-    },
-    condition,
-    function (result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    }
-  );
-});
-
-router.delete("/api/burgers/:id", function (req, res) {
-  var condition = "id = " + req.params.id;
-
-  cat.delete(condition, function (result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+router.post("/api/burgers", (req, res) => {
+  burger.insertOne("burger_name", req.body.burger_name, (result) => {
+    res.json({ id: result.insertId });
   });
 });
 
+router.put("/api/burgers/:id", (req, res) => {
+  let condition = `id = ${req.params.id}`;
+  burger.updateOne("devoured", 1, condition, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.json({ result });
+  });
+});
+
+router.delete("/api/burgers/:id", (req, res) => {
+  let condition = `id = ${req.params.id}`;
+  burger.deleteOne(condition, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.json({ result });
+  });
+});
 // Export routes for server.js to use.
 module.exports = router;
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,22 +1,27 @@
-// Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
+// Import ORM into the burger JS file
+const orm = require("../config/orm.js");
 
-var burger = {
+// The code that will call the ORM functions using burger specific input for the ORM.
+let burger = {
+  // add new burger to the data base
   insertOne: function (cols, vals, cb) {
     orm.insertOne("burgers", cols, vals, function (res) {
       cb(res);
     });
   },
+  // change the devoured status to true.
   updateOne: function (objColVals, boolean, condition, cb) {
     orm.updateOne("burgers", objColVals, boolean, condition, function (res) {
       cb(res);
     });
   },
+  // display all burger info in data base
   selectAll: function (cb) {
     orm.selectAll("burgers", function (res) {
       cb(res);
     });
   },
+  // delete the burger from the data base.
   deleteOne: function (condition, cb) {
     orm.deleteOne("burgers", condition, function (res) {
       cb(res);
@@ -24,5 +29,5 @@ var burger = {
   },
 };
 
-// Export the database functions for the controller (burgersController.js).
+// Export at the end of the burger file
 module.exports = burger;
