9f658fda115c7a820a7e2e1aa4cb3197bd05ccd4
Amy Paschke <pasch049@umn.edu>
2021-01-09 11:37:06 -0600

    changed password

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "ilovejosh3",
+    password: "vegetable",
     database: "burgers_db",
   });
 }
